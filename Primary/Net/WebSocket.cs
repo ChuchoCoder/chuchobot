c57e0efa25e74ad2bfdcfd4746333a5236479922
ChuchoCoder <traderchucho@gmail.com>
2023-12-26 16:54:15 -0300

    Build with VS2019

--- Primary/Net/WebSocket.cs
+++ Primary/Net/WebSocket.cs
@@ -6,120 +6,121 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Primary.Net;
-
-public class WebSocket<TRequest, TResponse> : IDisposable
+namespace Primary.Net
 {
-    internal WebSocket(Api api, TRequest request, CancellationToken cancelToken)
+    public class WebSocket<TRequest, TResponse> : IDisposable
     {
-        _api = api;
-        _request = request;
-        CancelToken = cancelToken;
-
-        var wsScheme = _api.BaseUri.Scheme == "https" ? "wss" : "ws";
-        UriBuilder uriBuilder = new(_api.BaseUri) { Scheme = wsScheme };
-        _uri = uriBuilder.Uri;
-    }
-
-    public async Task<Task> Start()
-    {
-        _client.Options.SetRequestHeader("X-Auth-Token", _api.AccessToken);
-
-        await _client.ConnectAsync(_uri, CancelToken);
-
-        // Send data to request
-        var jsonRequest = JsonConvert.SerializeObject(_request);
-
-        ArraySegment<byte> outputBuffer = new(Encoding.UTF8.GetBytes(jsonRequest));
-        await _client.SendAsync(outputBuffer, WebSocketMessageType.Text, true, CancelToken);
-
-        // Receive
-        var socketTask = Task.Factory.StartNew(ProcessSocketData, CancelToken,
-                                               TaskCreationOptions.LongRunning,
-                                               TaskScheduler.Default
-        );
-
-        return socketTask.Unwrap();
-    }
-
-    public bool IsRunning { get; private set; }
-    public CancellationToken CancelToken { get; }
-
-    public Action<Api, TResponse> OnData { get; set; }
-
-    #region IDisposable implementation
-
-    public void Dispose()
-    {
-        Dispose(true);
-        GC.SuppressFinalize(this);
-    }
-
-    protected virtual void Dispose(bool disposing)
-    {
-        if (disposing)
+        internal WebSocket(Api api, TRequest request, CancellationToken cancelToken)
         {
-            _client.Dispose();
+            _api = api;
+            _request = request;
+            CancelToken = cancelToken;
+
+            var wsScheme = _api.BaseUri.Scheme == "https" ? "wss" : "ws";
+            UriBuilder uriBuilder = new(_api.BaseUri) { Scheme = wsScheme };
+            _uri = uriBuilder.Uri;
         }
-    }
 
-    #endregion
-
-    private async Task ProcessSocketData()
-    {
-        IsRunning = true;
-
-        // Buffers for received data
-        List<byte> receivedMessage = new();
-        var buffer = new byte[9192];
-
-        while (true)
+        public async Task<Task> Start()
         {
-            try
+            _client.Options.SetRequestHeader("X-Auth-Token", _api.AccessToken);
+
+            await _client.ConnectAsync(_uri, CancelToken);
+
+            // Send data to request
+            var jsonRequest = JsonConvert.SerializeObject(_request);
+
+            ArraySegment<byte> outputBuffer = new(Encoding.UTF8.GetBytes(jsonRequest));
+            await _client.SendAsync(outputBuffer, WebSocketMessageType.Text, true, CancelToken);
+
+            // Receive
+            var socketTask = Task.Factory.StartNew(ProcessSocketData, CancelToken,
+                                                   TaskCreationOptions.LongRunning,
+                                                   TaskScheduler.Default
+            );
+
+            return socketTask.Unwrap();
+        }
+
+        public bool IsRunning { get; private set; }
+        public CancellationToken CancelToken { get; }
+
+        public Action<Api, TResponse> OnData { get; set; }
+
+        #region IDisposable implementation
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
             {
-                // Get data until the complete message is received
-                WebSocketReceiveResult response;
-                do
+                _client.Dispose();
+            }
+        }
+
+        #endregion
+
+        private async Task ProcessSocketData()
+        {
+            IsRunning = true;
+
+            // Buffers for received data
+            List<byte> receivedMessage = new();
+            var buffer = new byte[9192];
+
+            while (true)
+            {
+                try
                 {
-                    response = await _client.ReceiveAsync(new ArraySegment<byte>(buffer), CancelToken);
-                    if (response.CloseStatus != null)
+                    // Get data until the complete message is received
+                    WebSocketReceiveResult response;
+                    do
                     {
-                        throw new Exception(response.CloseStatusDescription);
-                    }
-                    ArraySegment<byte> segment = new(buffer, 0, response.Count);
-                    receivedMessage.AddRange(segment);
+                        response = await _client.ReceiveAsync(new ArraySegment<byte>(buffer), CancelToken);
+                        if (response.CloseStatus != null)
+                        {
+                            throw new Exception(response.CloseStatusDescription);
+                        }
+                        ArraySegment<byte> segment = new(buffer, 0, response.Count);
+                        receivedMessage.AddRange(segment);
 
-                } while (!response.EndOfMessage);
+                    } while (!response.EndOfMessage);
 
-                // Decode the message
-                var messageJson = Encoding.ASCII.GetString(receivedMessage.ToArray());
-                receivedMessage.Clear();
+                    // Decode the message
+                    var messageJson = Encoding.ASCII.GetString(receivedMessage.ToArray());
+                    receivedMessage.Clear();
 
-                // Parse and notify subscriber
-                var data = JsonConvert.DeserializeObject<TResponse>(messageJson);
-                OnData(_api, data);
-            }
-            catch (OperationCanceledException) { }
-            catch (WebSocketException wsEx)
-            {
-                throw wsEx;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
+                    // Parse and notify subscriber
+                    var data = JsonConvert.DeserializeObject<TResponse>(messageJson);
+                    OnData(_api, data);
+                }
+                catch (OperationCanceledException) { }
+                catch (WebSocketException wsEx)
+                {
+                    throw wsEx;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
 
-            if (CancelToken.IsCancellationRequested)
-            {
-                IsRunning = false;
-                CancelToken.ThrowIfCancellationRequested();
+                if (CancelToken.IsCancellationRequested)
+                {
+                    IsRunning = false;
+                    CancelToken.ThrowIfCancellationRequested();
+                }
             }
         }
+
+        private readonly ClientWebSocket _client = new();
+
+        private readonly TRequest _request;
+        private readonly Api _api;
+        private readonly Uri _uri;
     }
-
-    private readonly ClientWebSocket _client = new();
-
-    private readonly TRequest _request;
-    private readonly Api _api;
-    private readonly Uri _uri;
 }
