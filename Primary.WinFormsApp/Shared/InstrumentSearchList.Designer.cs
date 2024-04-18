42543ffaa527f2f8a0d9b7cbd95dae09a890f897
ChuchoCoder <traderchucho@gmail.com>
2024-04-18 09:46:14 -0300

    Fixed instruments layout

--- Primary.WinFormsApp/Shared/InstrumentSearchList.Designer.cs
+++ Primary.WinFormsApp/Shared/InstrumentSearchList.Designer.cs
@@ -28,57 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.chkOnlyCurrentPositions = new System.Windows.Forms.CheckBox();
-            this.txtSearch = new System.Windows.Forms.TextBox();
-            this.listInstrumentos = new System.Windows.Forms.ListBox();
-            this.SuspendLayout();
+            chkOnlyCurrentPositions = new System.Windows.Forms.CheckBox();
+            txtSearch = new System.Windows.Forms.TextBox();
+            listInstrumentos = new System.Windows.Forms.ListBox();
+            SuspendLayout();
             // 
             // chkOnlyCurrentPositions
             // 
-            this.chkOnlyCurrentPositions.AutoSize = true;
-            this.chkOnlyCurrentPositions.Location = new System.Drawing.Point(0, 3);
-            this.chkOnlyCurrentPositions.Name = "chkOnlyCurrentPositions";
-            this.chkOnlyCurrentPositions.Size = new System.Drawing.Size(196, 17);
-            this.chkOnlyCurrentPositions.TabIndex = 9;
-            this.chkOnlyCurrentPositions.Text = "Mostrar sólo instrumentos en cartera";
-            this.chkOnlyCurrentPositions.UseVisualStyleBackColor = true;
-            this.chkOnlyCurrentPositions.CheckedChanged += new System.EventHandler(this.chkOnlyCurrentPositions_CheckedChanged);
+            chkOnlyCurrentPositions.AutoSize = true;
+            chkOnlyCurrentPositions.Location = new System.Drawing.Point(0, 3);
+            chkOnlyCurrentPositions.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            chkOnlyCurrentPositions.Name = "chkOnlyCurrentPositions";
+            chkOnlyCurrentPositions.Size = new System.Drawing.Size(220, 19);
+            chkOnlyCurrentPositions.TabIndex = 9;
+            chkOnlyCurrentPositions.Text = "Mostrar sólo instrumentos en cartera";
+            chkOnlyCurrentPositions.UseVisualStyleBackColor = true;
+            chkOnlyCurrentPositions.CheckedChanged += chkOnlyCurrentPositions_CheckedChanged;
             // 
             // txtSearch
             // 
-            this.txtSearch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtSearch.Location = new System.Drawing.Point(0, 26);
-            this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(772, 20);
-            this.txtSearch.TabIndex = 2;
-            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            txtSearch.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            txtSearch.Location = new System.Drawing.Point(0, 28);
+            txtSearch.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new System.Drawing.Size(747, 23);
+            txtSearch.TabIndex = 2;
+            txtSearch.TextChanged += txtSearch_TextChanged;
             // 
             // listInstrumentos
             // 
-            this.listInstrumentos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.listInstrumentos.FormattingEnabled = true;
-            this.listInstrumentos.Location = new System.Drawing.Point(0, 51);
-            this.listInstrumentos.Name = "listInstrumentos";
-            this.listInstrumentos.Size = new System.Drawing.Size(772, 446);
-            this.listInstrumentos.TabIndex = 0;
-            this.listInstrumentos.DoubleClick += new System.EventHandler(this.listInstrumentos_DoubleClick);
+            listInstrumentos.FormattingEnabled = true;
+            listInstrumentos.ItemHeight = 15;
+            listInstrumentos.Location = new System.Drawing.Point(0, 57);
+            listInstrumentos.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            listInstrumentos.Name = "listInstrumentos";
+            listInstrumentos.Size = new System.Drawing.Size(747, 529);
+            listInstrumentos.TabIndex = 0;
+            listInstrumentos.DoubleClick += listInstrumentos_DoubleClick;
             // 
             // InstrumentSearchList
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.listInstrumentos);
-            this.Controls.Add(this.txtSearch);
-            this.Controls.Add(this.chkOnlyCurrentPositions);
-            this.Name = "InstrumentSearchList";
-            this.Size = new System.Drawing.Size(772, 505);
-            this.Load += new System.EventHandler(this.InstrumentSearchList_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            Controls.Add(listInstrumentos);
+            Controls.Add(txtSearch);
+            Controls.Add(chkOnlyCurrentPositions);
+            Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            Name = "InstrumentSearchList";
+            Size = new System.Drawing.Size(747, 594);
+            Load += InstrumentSearchList_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
